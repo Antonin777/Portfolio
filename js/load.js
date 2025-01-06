fdab4851008dd967c377530553d1505c85a62a6e
Antonin777 <antonin.merzougui@gmail.com>
2025-01-06 23:11:16 +0100

    Animation de chargement

---
+++ js/load.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const progressText = document.getElementById("progress-text");
+    let progress = 0;
+
+    // Simulation de chargement
+    const interval = setInterval(() => {
+        progress += 5;
+        progressText.textContent = `${progress}%`;
+
+        if (progress >= 100) {
+            clearInterval(interval);
+            const loader = document.getElementById("loader");
+            setTimeout(() => {
+                loader.style.opacity = "0";
+                loader.style.visibility = "hidden";
+            }, 500); // Masque le loader après 0,5 seconde
+        }
+    }, 200);
+});
